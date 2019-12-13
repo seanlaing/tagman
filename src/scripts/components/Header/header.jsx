cb06a15976d051467ddb5bb12207930200fc9709
Sean Laing <sean.laing@meredith.com>
2019-12-13 11:04:02 -0800

    Finished last couple fixs

--- src/scripts/components/Header/header.jsx
+++ src/scripts/components/Header/header.jsx
@@ -5,11 +5,25 @@ class Header extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {points: 0, playing: true};
+		this.state = {points: 0, playing: true, timer:30};
 	}
 
 	componentDidMount() {
 		this.container = ReactDOM.findDOMNode(this);
+		const countDown = setInterval(() =>{
+			this.setState({
+				timer: this.state.timer-1
+			}) 
+		},1000);
+	}
+	componentDidUpdate(){
+		if (this.state.timer<1){
+			window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+			clearInterval(countDown);
+		}
+	}
+	componentWillUnmount() {
+		clearInterval(countDown);
 	}
 
 	increase() {
@@ -28,6 +42,7 @@ class Header extends React.Component {
 			<div className="header">
 				<span className="left title">TAGMAN</span>
 				<span className="right score">TAGS: <span className="points">{this.state.points}</span></span>
+				<span className="middle timer">TIMER: <span className="timer">{this.state.timer}</span></span>
 			</div>
 		);
 	}
